4d42330d5df0f9e8ab15a2d47c6d7bba23aad133
Christophe Pettus <cpettus@pgexperts.com>
2012-03-28 19:32:20 -0700

    Now, with hot data-size action.

--- noisewords.py
+++ noisewords.py
@@ -10,8 +10,17 @@ parser.add_option('', "--columns", action="store", type="int", dest="columns", d
 parser.add_option('', "--min-length", action="store", type="int", dest="min_length", default=8)
 parser.add_option('', "--max-length", action="store", type="int", dest="max_length", default=255)
 parser.add_option('', "--delimiter", action="store", type="string", dest="delimiter", default=',')
+parser.add_option('', "--data-size", action="store", type="int", dest="data_size", default=0)
 
 (options, args) = parser.parse_args()
 
+remaining = options.data_size
+
 for row in xrange(options.rows):
-    print options.delimiter.join([''.join([random.choice(alphabet) for i in xrange(random.randint(options.min_length, options.max_length))]) for j in xrange(options.columns)])
+    line = options.delimiter.join([''.join([random.choice(alphabet) for i in xrange(random.randint(options.min_length, options.max_length))]) for j in xrange(options.columns)])
+    print line
+    
+    if remaining:
+        remaining -= len(line)
+        if remaining <= 0:
+            break
